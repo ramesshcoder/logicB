1ba5f33c0e1e54b68057114a105f36733af023ef
ramesshcoder <rameshkumarer321@gmail.com>
2025-07-18 10:19:28 +0530

    new

--- smallest element.js
+++ smallest element.js
@@ -1,10 +1,13 @@
-let arr = [56,9,7,4,3,];
-let smallest = arr[0];
-for (let i = 1; i < arr.length; i++) {
-    if(
-        arr[i]>smallest
-    ){
+let arr=[12,87,45,34,45];
+let smallest=arr[0];
+let largest=arr[0]
+for(let i=1;i<arr.length;i++){
+    if(arr[i]<smallest){
         smallest=arr[i]
     }
+    if(arr[i]>largest){
+        largest=arr[i]
+    }
 }
-console.log(smallest,": is smallest number in given array");
+console.log("smallest number is :",smallest);
+console.log("largest number is :",largest);
