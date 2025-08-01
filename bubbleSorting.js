4b711a016a51ae26c01d1b221dfa154ed04f6074
ramesshcoder <rameshkumarer321@gmail.com>
2025-08-01 07:21:21 +0530

    bubble sort

--- bubbleSorting.js
+++ bubbleSorting.js
@@ -1,13 +1,13 @@
-let arr=[10,50,60,30,40,90,5];
+// bubble sort
+let arr=[12,32,56,89,76,0];
 let n=arr.length;
-for (let i=0;i<=n-1;i++){
+for(let i=1;i<=n-1;i++){
     for(let j=0;j<=n-1-i;j++){
-        if(arr[j]<arr[j+1]){
+        if(arr[j]>arr[j+1]){
             let temp=arr[j];
             arr[j]=arr[j+1];
             arr[j+1]=temp;
         }
-
     }
 }
-console.log("sorted array is ",arr);
+console.log( "sorted array is",arr);
